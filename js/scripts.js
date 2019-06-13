5492dd6517ec3e5095387cac14f6fbe76b790240
Erik Irgens <erik.t.irgens@gmail.com>
2019-06-13 11:47:47 -0700

    Add more functionality to remove [index] within message if it is puntuation or space. Doesn't seem to work, using .splice(); method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,44 @@
 
 $(document).ready(function() {
     $("form#cryptoForm").submit(function(event) {
-        event.preventDefault();
-    });
-    var userInput = $("#userInput").val();
+        var userInput = $("#userInput").val().toLowerCase();
 
-    var messageCharacters = userInput.split("");
+        if (userInput === "") {
+            alert("Please enter a sentence!"); 
+        } else {
+            var messageCharacters = userInput.split("");
+       
+            debugger;
+            for (var index = 0; index < messageCharacters.length; index++) {  
+                if (messageCharacters[index] === "." || messageCharacters[index] === "," || messageCharacters[index] === "!"  || messageCharacters[index] === "?" || messageCharacters[index] === " ") {
+                    messageCharacter.splice(index, 1,);
+                }
 
-    for (var index = 0; index < messageCharacters.length; index++) {
-        if (messageCharacters[index] === "." || messageCharacters[index] === "," || messageCharacters[index] === "!"  || messageCharacters[index] === "?" || messageCharacters[index] === " ") {
-            messageCharacter[index] = "";
+
+                
+            }
+            
+            messageCharacters = messageCharacters.join("").split("");
+
+            alert(messageCharacters);
+            
+            var messageLength = messageCharacters.length;
+            
+            var squareSize = 0;
+            for (var index = 1; (index*index) < messageLength; index++ ){
+                squareSize = index;
+            }
+            
+            for (var index = 0; squareSize >= messageLength; index++){
+                messageCharacters[index]
+            }
         }
         
-    }
-
-    messageCharacters = messageCharacters.join("").split("");
-
-    var messageLength = messageCharacters.length;
-
-    var squareSize = 0;
-    for (var index = 1; (index*index) < messageLength; index++ ){
-        squareSize = index;
-    }
-
-    for (var index = 0; squareSize >= messageLength; index++){
-        messageCharacters[index]
-    }
-    
-
-    
+        
+        
+        
+        
+        event.preventDefault();
+    });
 });
 
